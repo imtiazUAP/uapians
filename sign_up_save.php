6734008fc404069f376c5237f8677551bf20b9e2
imtiazUAP <imtiaz.uap@gmail.com>
2016-06-29 01:03:20 +0600

    Photo Size Reduce & Rename
    Photo Size Reducing automatically & renaming the photo

--- sign_up_save.php
+++ sign_up_save.php
@@ -1,57 +1,47 @@
 <?php
 error_reporting(0);
-include("dbconnect.php"); ?>
-<?php
-$allowedExts = array("gif", "jpeg", "jpg", "png");
-$temp = explode(".", $_FILES["file"]["name"]);
-$extension = end($temp);
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& in_array($extension, $allowedExts))
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-    }
-  else
-    {
-	//echo "Thank You for Registration. please wait for Admin Review (maximum 24 hour)";
-    					//echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    					//echo "Type: " . $_FILES["file"]["type"] . "<br>";
-   					//echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-					//echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
+include("dbconnect.php");
 
-    if (file_exists("images/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists please rename your photo ";
-      }
-      
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "images/" . $_FILES["file"]["name"]);
-      
-      					//echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-	  				//$d = dir(getcwd());
-	  
-	  $a='images/'.$_FILES["file"]["name"];
-	  				//echo $a;
-	  mysql_query($sql="INSERT INTO sign_up (SID,SPortrait,SName,SReg,SHouse,district_id,SPh_Number,SE_Mail,SB_of_Date,SMID,Blood_Group_ID,donor_value,School, College, FB_Link,username,password)VALUES ('','$a','" . $_REQUEST['name'] . "','" . $_REQUEST['reg'] . "','" . $_REQUEST['house'] . "','" . $_REQUEST['district_id'] . "','" . $_REQUEST['contact'] . "','" . $_REQUEST['email'] . "','" . $_REQUEST['dob'] . "','" . $_REQUEST['SMID'] . "','" . $_REQUEST['Blood_Group_ID'] . "','" . $_REQUEST['donor_value'] . "','" . $_REQUEST['School'] . "','" . $_REQUEST['College'] . "','" . $_REQUEST['FB_Link'] . "','" . $_REQUEST['username'] . "','" . $_REQUEST['password'] . "'
-	  )");
-	  
-      }
+$post_photo=$_FILES['file']['name'];
+$post_photo_tmp=$_FILES['file']['tmp_name'];
+
+$ext = pathinfo($post_photo, PATHINFO_EXTENSION);  // getting image extension 
+
+if(   $ext=='png' || $ext=='PNG' || $ext=='jpg' || $ext=='jpeg' || $ext=='JPG' || $ext=='JPEG' || $ext=='gif' || $ext=='GIF'  ) {
+
+    if($ext=='jpg' || $ext=='jpeg' || $ext=='JPG' || $ext=='JPEG')
+    {
+        $src=imagecreatefromjpeg($post_photo_tmp);
     }
-  }
-else
-  {
-  echo "Registration Failed.... <br>Please Fill all the field Correctly";
-  }
-  
-  
-  header ('location: https://localhost/uapians/sign_up_notification.php ');
+    if($ext=='png' || $ext=='PNG')
+    {
+        $src=imagecreatefrompng($post_photo_tmp);
+    }
+    if($ext=='gif' || $ext=='GIF')
+    {
+        $src=imagecreatefromgif($post_photo_tmp);
+    }
+
+    list($width_min,$height_min)=getimagesize($post_photo_tmp); // fetching original image width and height
+
+    $newwidth_min=350; // set compressing image width
+
+    $newheight_min=($height_min / $width_min) * $newwidth_min; // equation for compressed image height
+
+    $tmp_min = imagecreatetruecolor($newwidth_min, $newheight_min); // create frame  for compress image
+
+    imagecopyresampled($tmp_min, $src, 0,0,0,0,$newwidth_min, $newheight_min, $width_min, $height_min); // compressing image
+
+    $newfilename = round(microtime(true)) . '.' . $ext; // creating a new file name as 2 photo can not be with same name
+
+    imagejpeg($tmp_min,"images/".$newfilename,80); //copy image in folder//
+    $photo_name = 'images/'.$newfilename; // new name with path to save in database
+
+    mysql_query($sql="INSERT INTO sign_up (SID,SPortrait,SName,SReg,district_id,SE_Mail,SMID,Blood_Group_ID,donor_value,username,password)VALUES ('','$photo_name','" . $_REQUEST['name'] . "','" . $_REQUEST['reg'] . "','" . $_REQUEST['district_id'] . "','" . $_REQUEST['email'] . "','" . $_REQUEST['SMID'] . "','" . $_REQUEST['Blood_Group_ID'] . "','" . $_REQUEST['donor_value'] . "','" . $_REQUEST['username'] . "','" . $_REQUEST['password'] . "')");
+} else {
+    echo "Registration Failed.... <br>Please Fill all the field Correctly";
+}
+
+header ('location: https://localhost/uapians/sign_up_notification.php ');
 mysql_close($con)
 ?> 
