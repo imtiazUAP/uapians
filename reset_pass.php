5b93dd94525a32839411235043e7feab0c8f47f9
emtiaj <imtiaz.uap@gmail.com>
2016-09-03 04:04:43 +0600

    reset password exception handled
    Reset password exception handled

--- reset_pass.php
+++ reset_pass.php
@@ -9,7 +9,7 @@
 	}
 ?>
 
-<html>
+<html xmlns="http://www.w3.org/1999/html">
     <head>
         <?php include("header.php");?>
     </head>
@@ -31,73 +31,124 @@
         <br>
 		
 		<?php
-		if(isset($_GET['action']))
-		{ 
-			if($_GET['action']=="reset")
-			{
-				$encrypt = $_GET['encrypt'];
-				$query = "SELECT id FROM users where recovery='".$encrypt."'";
-				$result  = mysql_query($query,$con);
-				$Results = mysql_fetch_array($result);
+        $message  = $_GET['message'];
 
-				if(count($Results[id])>=1) {
-				?>
-				
-	<body>
-	<div align="center">
-		</br>               
-			<form action="reset_pass.php" method="post" id="reset" >
-				<table align="center">
-					<tr>
-						<td><label for="email" class="signup_field" data-icon="u">Enter a new password: </label></td>
-						<td><input id="password" name="password" required="required" type="password" placeholder="new password"></td>
-						<input name="action" type="hidden" value='<?= $encrypt ?>'>
-						<td><button name="login" type="Submit" class="button button_blue">Reset Password</button></td>
-					</tr>
-				</table>
-			</form>
-			</div>
-	
-			<?php } else
-				{
-					echo 'Invalid key please try again. <a href="reset_pass.php">Forget Password?</a>';
-				}
-			}
-		}
-		elseif(isset($_POST['action'])){
-			$encrypt      = $_POST['action'];
-			$password     = $_POST['password'];
-			$query = "SELECT id FROM users where recovery = '".$encrypt."'";
-			
-			$result  = mysql_query($query,$con);
-			$Results = mysql_fetch_array($result);
-				
-			if(count($Results[id])>=1){
-				$query = "update users set password='".md5($password)."' where id='".$Results['id']."'";
-				mysql_query($query,$con);
+        if($message == 'confirmation_mail'){
+        ?>
+            </br>
+            </br>
+            <div align="center">
+                <label style="color: white; font-size:14px;">A confirmation mail sent to your <span style="color: #ff0000">Email address.</span> </br> Log in to your Email & click on that <span style="color: #50B9E8">link</span> to reset your password.</label>
+            </div>
+        <?php
+        }elseif($message == 'account_not_found'){
+            ?>
+            </br>
+            </br>
+            <div align="center">
+                <label style="color: white; font-size:14px;">Account not found with this <span style="color: #ff0000">Email address.</span>. Please...  <a style="color: #ff0000" href="sign_up.php">Sign Up</a> to continue... </br> or <a style="color: #ff0000" href="reset_pass.php">click here</a> to try with a different Email address</label>
+            </div>
+        <?php
+        }elseif($message == 'wrong_login_id__or_pass'){
+            ?>
+            </br>
+            </br>
+            <div align="center">
+                <label style="color: white; font-size:14px;">Opps!! <span style="color: #ff0000">wrong login info</span> provided. Please try again or if you forgot your password... </br> <a style="color: #50B9E8" href="reset_pass.php">click here to reset your password</a></label>
+            </div>
+        <?php
+        }else{
 
-				echo 'Your password changed sucessfully <a href="index.php">click here to login</a>.';
-			}else{
-				echo 'Invalid key please try again. <a href="reset_pass.php">Forget Password?</a>';
-			}
-		}else{
-			?>
-			
-			<div align="center">
-				<form action="reset_pass_request.php" method="post" enctype="multipart/form-data">
-					<table align="center">
-						<tr>
-							<td><label for="email" class="signup_field" data-icon="u">Your E Mail:</label> </td>
-							<td><input id="lastnamesignup" name="email" required="required" type="text" placeholder="example@yahoo.com" /></td>
-							<td><button type="Submit" class="button button_blue">Reset Password</button></td>
-						</tr>
-					</table>
-					<br>
-					<br>
-				</form>
-			</div>
-			
-			<?php } ?>		
+        if(isset($_GET['action']))
+        {
+        if($_GET['action']=="reset")
+        {
+        $encrypt = $_GET['encrypt'];
+        $query = "SELECT userid FROM userinfo where recovery='".$encrypt."'";
+        $result  = mysql_query($query,$con);
+        $Results = mysql_fetch_array($result);
+
+        if(count($Results[userid])>=1) {
+        ?>
+
+            <body>
+            <div align="center">
+                </br>
+                <form action="reset_pass.php" method="post" id="reset" >
+                    <table align="center">
+                        <tr>
+                            <td><label for="email" class="signup_field" data-icon="u">Enter a new password: </label></td>
+                            <td><input id="password" name="password" required="required" type="password" placeholder="new password"></td>
+                            <input name="action" type="hidden" value='<?= $encrypt ?>'>
+                            <td><button name="login" type="Submit" class="button button_blue">Reset Password</button></td>
+                        </tr>
+                    </table>
+                </form>
+            </div>
+
+            <?php } else
+            {
+                ?>
+                </br>
+                </br>
+                <div align="center">
+                    <label style="color: white; font-size:14px;">Invalid verification link, or this verification link is already used one time. </br> Please try again...  <a style="color: #ff0000" href="reset_pass.php">Forget Password?</a></label>
+                </div>
+            <?php
+            }
+            }
+            }
+            elseif(isset($_POST['action'])){
+                $encrypt      = $_POST['action'];
+                $password     = $_POST['password'];
+                $query = "SELECT userid FROM userinfo where recovery = '".$encrypt."'";
+
+                $result  = mysql_query($query,$con);
+                $Results = mysql_fetch_array($result);
+
+                if(count($Results[userid])>=1){
+                    $randomNumber = date("Y-m-d H:i:s");
+                    $newEncrypt = md5($randomNumber*209*19+$Results['userid']);
+                    $query = "update userinfo set password='".md5($password)."', recovery='".$newEncrypt."' where userid='".$Results['userid']."'";
+                    mysql_query($query,$con);
+                    ?>
+                    </br>
+                    </br>
+                    <div align="center">
+                        <label style="color: white; font-size:14px;">Your password changed sucessfully...  <a style="color: #ff0000" href="index.php">click here to login</a></label>
+                    </div>
+                <?php
+                }else{
+                    ?>
+                    </br>
+                    </br>
+                    <div align="center">
+                        <label style="color: white; font-size:14px;">Invalid verification link, or this verification link is already used one time. </br> Please try again...  <a style="color: #ff0000" href="reset_pass.php">Forget Password?</a></label>
+                    </div>
+                <?php
+                }
+            }else{
+                ?>
+
+                <div align="center">
+                    <form action="reset_pass_request.php" method="post" enctype="multipart/form-data">
+                        <table align="center">
+                            <tr>
+                                <td><label for="email" class="signup_field" data-icon="u">Your E Mail:</label> </td>
+                                <td><input id="lastnamesignup" name="email" required="required" type="text" placeholder="example@yahoo.com" /></td>
+                                <td><button type="Submit" class="button button_blue">Reset Password</button></td>
+                            </tr>
+                        </table>
+                        <br>
+                        <br>
+                    </form>
+                </div>
+
+            <?php }
+
+        }
+            ?>
+
 		
 		</div>
 		</div>
--- reset_pass_request.php
+++ reset_pass_request.php
@@ -7,30 +7,34 @@ if(!$con)
 	mysql_select_db("uapians_mylab",$con);
 
 	$email  = $_POST['email'];
-	$query = "SELECT id FROM users where email='".$email."'";
+	$query = "SELECT userid FROM userinfo where SE_Mail='".$email."'";
 	$result  = mysql_query($query,$con);
 	$Results = mysql_fetch_array($result);
-	$id      = $Results[id];	
+	$id      = $Results[userid];
  
 	if(count($id)>=1){
-		$encrypt = md5(90*13+$Results['id']);
+        $randomNumber = date("Y-m-d H:i:s");
+		$encrypt = md5($randomNumber*109*19+$Results['userid']);
 		$message = "Your password reset link send to your e-mail address.";
 		$to=$email;
 		$subject="Forget Password";
 		$from = 'emtiaj@yahoo.com';
-		$body='Hi, <br/> <br/>Your Membership ID is '.$Results['id'].' <br><br>Click here to reset your password https://localhost/uapians/reset_pass.php?encrypt='.$encrypt.'&action=reset   <br/> <br/>--<br>UAPians.<br>Solve your problems.';
+		$body='Hi, <br/> <br/>Your Membership ID is '.$Results['userid'].' <br><br>Click here to reset your password https://localhost/uapians/reset_pass.php?encrypt='.$encrypt.'&action=reset   <br/> <br/>--<br>UAPians.Net Team<br>Solve your problems.';
 		$headers = "From: " . strip_tags($from) . "\r\n";
 		$headers .= "Reply-To: ". strip_tags($from) . "\r\n";
 		$headers .= "MIME-Version: 1.0\r\n";
 		$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 		
-		$query = "update users set recovery='".$encrypt."' where id='".$Results['id']."'";
+		$query = "update userinfo set recovery='".$encrypt."' where userid='".$Results['userid']."'";
 		mysql_query($query,$con);
 		
 		mail($to,$subject,$body,$headers);
 
+        header ('location:reset_pass.php?message=confirmation_mail');
+
 	}else {
-		echo "Account not found please signup now!!";	
+        header ('location:reset_pass.php?message=account_not_found');
+
 	}
 
 mysql_close($con)
